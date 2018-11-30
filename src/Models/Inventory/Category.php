a67630545100daad7f65e0b4838a4335eeafa74e
Wei <weic102193@gmail.com>
2018-11-30 18:38:18 -0500

    create association api

---
+++ src/Models/Inventory/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Guesl\Clover\Models\Inventory;
+
+
+use Guesl\Clover\Models\Clover;
+
+/**
+ * Class Category
+ * @package Guesl\Clover\Models\Inventory
+ */
+class Category extends Clover
+{
+    /**
+     * Create a item category
+     *
+     * @param array $categoryData
+     * @return mixed
+     */
+    public static function create($categoryData = [])
+    {
+        $httpClient = self::getHttpClient();
+        $merchantId = self::getMerchantId();
+        $version = self::VERSION;
+
+        $category = $result = $httpClient->post("$version/merchants/$merchantId/categories", [
+            'json' => $categoryData,
+        ]);
+
+        return $category;
+    }
+}
